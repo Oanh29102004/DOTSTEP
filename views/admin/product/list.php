fba2d82beb0ff31396e3895194224700ae38cca2
Oanh29102004 <ngocoanh2202200418@gmail.com>
2024-11-20 09:55:39 +0700

    feat:product-list

--- views/admin/product/list.php
+++ views/admin/product/list.php
@@ -16,12 +16,13 @@
                         <table id="myDataTable" class="table table-hover align-middle mb-0" style="width: 100%;">
                             <thead>
                                 <tr>
-                                    <th style="width: 20px;">
+                                    <!-- <th style="width: 20px;">
                                         <div class="form-check ms-1">
                                             <input type="checkbox" class="form-check-input" id="customCheck1">
                                             <label class="form-check-label" for="customCheck1"></label>
                                         </div>
-                                    </th>
+                                    </th> -->
+                                    <th>ID</th>
                                     <th>Tên sản phẩm và biến thể</th>
                                     <th>Danh mục</th>
                                     <th>Giá sản phẩm</th>
@@ -30,27 +31,38 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php foreach($listProduct as $product) : ?>
                                 <tr>
-                                    <td>
+                                    <!-- <td>
                                         <div class="form-check ms-1">
                                             <input type="checkbox" class="form-check-input" id="customCheck2">
                                             <label class="form-check-label" for="customCheck2">&nbsp;</label>
                                         </div>
-                                    </td>
+                                    </td> -->
+                                    <td><?= $product['product_id'] ?></td>
                                     <td>
                                         <div class="d-flex align-items-center gap-2">
                                             <div class="rounded bg-light avatar-md d-flex align-items-center justify-content-center">
                                                 <img src="admin/assets/images/product/product-1.jpg" alt="" class="w120 rounded img-fluid">
                                             </div>
                                             <div>
-                                                <a href="#!" class="text-dark fw-medium fs-15">Black T-shirt</a>
-                                                <p class="text-muted mb-0 mt-1 fs-13"><span>Size : </span>S , M , L , Xl </p>
+                                                <a href="#!" class="text-dark fw-medium fs-15"><?=$product['product_name']?></a>
+                                                <p class="text-muted mb-0 mt-1 fs-13"><span>Size : </span>
+                                                    <?php foreach ($product['variants'] as $size) : ?>
+                                                        <span><?= $size['product_variant_size']  ?></span>
+                                                    <?php endforeach ?>                                        
+                                                </p>
+                                                <p class="text-muted mb-0 mt-1 fs-13"><span>Color : </span>
+                                                    <?php foreach ($product['variants'] as $color) : ?>
+                                                        <span><?= $size['product_variant_color'] ?></span>
+                                                    <?php endforeach ?>                                        
+                                                </p>
                                             </div>
                                         </div>
                                     </td>
-                                    <td>Football</td>
-                                    <td>290000 Vnđ</td>
-                                    <td>222000 Vnđ</td>
+                                    <td><?= $product['category_name'] ?></td>
+                                    <td><?= $product['product_price'] ?></td>
+                                    <td><?= $product['product_sale_price'] ?></td>
                                     <td>
                                         <div class="btn-group" role="group" aria-label="Basic outlined example">
                                             <a href="categories-edit.html" class="btn btn-outline-secondary"><i class="icofont-edit text-success"></i></a>
@@ -58,6 +70,8 @@
                                         </div>
                                     </td>
                                 </tr>
+                                <?php endforeach ?>
+                            </tbody>    
                                 
                         </table>
                     </div>
